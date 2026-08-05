b7c8a1227a162a43c2ffdf7bb6222fc952efb76a
defoltusername <mosyanarayik777@gmail.com>
2026-08-05 19:34:23 +0400

    Feature - News App created

--- back/apps/news/views.py
+++ back/apps/news/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import ListView, DetailView
+from .models import Article, Category
 
 
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = "articles"
 
-def products(request):
+    def get_queryset(self):
+        return (
+            Article.objects.filter(status=Article.PUBLISHED)
+            .select_related("author", "category")
+            .order_by("-published_at")
+        )
 
-    products = Product.objects.all()
 
-    return render(
-        request,
-        "products.html",
-        {
-            "products": products
-        }
-    )
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = "article"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+
+    def get_queryset(self):
+        return Article.objects.filter(
+            status=Article.PUBLISHED
+        ).select_related("author", "category")
+
+
+class CategoryListView(ListView):
+    model = Category
+    context_object_name = "categories"
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    context_object_name = "category"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
