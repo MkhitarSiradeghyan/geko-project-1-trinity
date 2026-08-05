ea9ae807df772f1aa3aacc1c2b82e27bfcc82993
mhambardzumyan675_ui <mhambardzumyan675@gmail.com>
2026-08-05 23:49:44 +0400

    Add tours API backend implementation

---
+++ back/apps/tours/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Tour
+
+class TourSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tour
+        fields = [
+            "id",
+            "title",
+            "price",
+            "created_at"
+        ]
+
+    def validate(self, data):
+        title = data.get("title")
+        price = data.get("price")
+
+        if title is not None:
+            if title.strip() == "":
+                raise serializers.ValidationError(
+                    {"title": "Title cannot be empty."}
+                )
+            
+        if price is not None:
+            if price <= 0:
+                raise serializers.ValidationError(
+                    {"price": "Price must be greater than 0."}
+                )
+        return data
--- back/config/settings/base.py
+++ back/config/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     
     "rest_framework",
     "corsheaders",
+    'apps.tours',
 ]
 UNFOLD = {
     "SITE_TITLE": "Trinity Travel Boutique",
