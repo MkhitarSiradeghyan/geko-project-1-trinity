c0db68e6ed450b6e9f5d92c05658a06367bb74c4
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-08-07 18:34:08 +0400

    Refactor - Tours code formatting

--- back/apps/tours/urls.py
+++ back/apps/tours/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tours/",views.tours_list,name="tours-list"),
-    path("tours/<int:id>/",views.tour_detail,name="tour-detail"),
-
-              ]
+    path("", views.tours_list, name="tours-list"),
+    path("<int:id>/", views.tour_detail, name="tour-detail"),
+]
--- back/config/urls.py
+++ back/config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/",include("apps.tours.urls")),
+    path("api/tours/",include("apps.tours.urls")),
 
 ]
