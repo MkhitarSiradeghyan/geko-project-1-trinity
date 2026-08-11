948c86404677ff5fb4c66b49c53ed3a8ab0d88a7
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-08-11 20:27:41 +0400

    Hotfix - Arayik's borsch fix

--- back/apps/users/urls.py
+++ back/apps/users/urls.py
@@ -11,7 +11,6 @@ from .views import (
 
 
 urlpatterns = [
-
     path("register/", register, name="register"),
     path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
