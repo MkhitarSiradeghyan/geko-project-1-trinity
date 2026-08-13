d519d28af4823d2dfd1e9b7b5cd3cea9aacbb61c
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-08-13 15:57:46 +0400

    Fix - front structure fix

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import "./styles/global.sass";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
