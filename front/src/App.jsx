ced3b51501c9f294d483820eae818bc2e0248735
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-08-14 18:28:10 +0400

    Fix - requirements.txt

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -19,16 +19,16 @@ function App() {
   return(
     <Routes>
       <Route path='/' element = {<Home />} />
-      <Route path='About' element = {<About />} />
-      <Route path='Tours' element = {<Tours />} />
-      <Route path='TourDetails' element = {<TourDetails />} />
-      <Route path='Gallery' element = {<Gallery />} />
-      <Route path='News' element = {<News />} />
-      <Route path='NewsDetails' element = {<NewsDetails />} />
-      <Route path='Contacts' element = {<Contacts />} />
-      <Route path='ThankYou' element = {<ThankYou />} />
-      <Route path='Privacy' element = {<Privacy />} />
-      <Route path='Terms' element = {<Terms />} />
+      <Route path='/About' element = {<About />} />
+      <Route path='/Tours' element = {<Tours />} />
+      <Route path='/TourDetails' element = {<TourDetails />} />
+      <Route path='/Gallery' element = {<Gallery />} />
+      <Route path='/News' element = {<News />} />
+      <Route path='/NewsDetails' element = {<NewsDetails />} />
+      <Route path='/Contacts' element = {<Contacts />} />
+      <Route path='/ThankYou' element = {<ThankYou />} />
+      <Route path='/Privacy' element = {<Privacy />} />
+      <Route path='/Terms' element = {<Terms />} />
       
       {/*404*/}
       <Route path='*' element = {<NotFound />} />
