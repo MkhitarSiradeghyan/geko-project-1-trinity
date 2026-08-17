b7b60c54236c71fc55931495246193914684a4be
defoltusername <mosyanarayik777@gmail.com>
2026-08-17 18:10:00 +0400

    Feature - Merge

--- back/config/urls.py
+++ back/config/urls.py
@@ -10,10 +10,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/",include("apps.tours.urls")),
-    path("api/",include("apps.gallery.urls")),
-
-
     path("api/schema/",SpectacularAPIView.as_view(),name="schema",),
     path("api/docs/",SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui",),
     path("api/redoc/",SpectacularRedocView.as_view(url_name="schema"),name="redoc",),
